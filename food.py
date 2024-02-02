0a2084d821d2332c9dde333a7707129971b45de9
Vojtech Mikel <tami.vojkel@gmail.com>
2024-02-02 18:30:38 +0100

    add inheritance from Turtle to Food

--- food.py
+++ food.py
@@ -3,20 +3,16 @@ from turtle import Turtle
 import random
 
 
-class Food:
+class Food(Turtle):
     def __init__(self):
-        self.turtle = Turtle()
-        self.turtle.shape("circle")
-        self.turtle.color("#1F6357")
-
+        super().__init__()
+        self.shape("circle")
+        self.color("#1F6357")
         self.place()
-
-    def pos(self):
-        return self.turtle.pos()
 
     def place(self):
         xcor = random.randint(-14, 14) * 20
         ycor = random.randint(-14, 14) * 20
-        self.turtle.teleport(xcor, ycor)
+        self.teleport(xcor, ycor)
 
 
