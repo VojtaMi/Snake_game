4795727d59e7f8481a30ccc631e9fb97c953460e
Vojtech Mikel <tami.vojkel@gmail.com>
2024-02-02 18:28:01 +0100

    inheritance added to segment, code simplified

--- segment.py
+++ segment.py
@@ -8,43 +8,19 @@ class Direction:
     DOWN = 270
 
 
-class Segment:
+class Segment(Turtle):
     def __init__(self):
-        self.turtle = Turtle()
-        self.turtle.penup()
-        # self.turtle.shape("square")
-        # self.turtle.color("blue")
-        self.turtle.hideturtle()
-
+        super().__init__()
+        self.penup()
+        self.hideturtle()
         self.next_segment = None
         self.previous_segment = None
 
     def pin(self, next_segment, grid_size):
-        x, y = self.turtle.position()
-        direction = self.get_heading()
+        next_segment.setheading(self.heading())
+        next_segment.setpos(self.pos())
+        next_segment.back(grid_size)
 
-        if direction == Direction.RIGHT:
-            next_segment.turtle.setposition(x - grid_size, y)
-        elif direction == Direction.LEFT:
-            next_segment.turtle.setposition(x + grid_size, y)
-        elif direction == Direction.UP:
-            next_segment.turtle.setposition(x, y - grid_size)
-        elif direction == Direction.DOWN:
-            next_segment.turtle.setposition(x, y + grid_size)
-
-        next_segment.set_heading(direction)
         self.next_segment = next_segment
         next_segment.previous_segment = self
-
-    def set_heading(self, angle):
-        self.turtle.setheading(angle)
-
-    def get_heading(self):
-        return self.turtle.heading()
-
-    def move(self, grid_size):
-        self.turtle.forward(grid_size)
-
-    def pos(self):
-        return self.turtle.pos()
 
--- snakebody.py
+++ snakebody.py
@@ -30,7 +30,7 @@ class SnakeBody:
         self.tail = new_tail
 
     def turn(self, direction):
-        heading = self.head.get_heading()
+        heading = self.head.heading()
         if (heading == Direction.UP or heading == Direction.DOWN) and (
                 direction == Direction.LEFT or direction == Direction.RIGHT):
             self.head_switch = direction
@@ -49,7 +49,7 @@ class SnakeBody:
         draw_body_turtle.pendown()
         # draw main body
         while from_segment is not self.head:
-            draw_body_turtle.setheading(from_segment.get_heading())
+            draw_body_turtle.setheading(from_segment.heading())
             draw_body_turtle.forward(GRID_SIZE)
             from_segment = from_segment.previous_segment
 
@@ -64,14 +64,14 @@ class SnakeBody:
             draw_ht_turtle.penup()
             draw_ht_turtle.goto(new_tail_pos)
             draw_ht_turtle.pendown()
-            draw_ht_turtle.setheading(self.tail.get_heading())
+            draw_ht_turtle.setheading(self.tail.heading())
             draw_ht_turtle.back(complement_fraction_distance)
 
             # grow head
             draw_ht_turtle.penup()
             draw_ht_turtle.goto(self.head.pos())
             draw_ht_turtle.pendown()
-            draw_ht_turtle.setheading(self.head.get_heading())
+            draw_ht_turtle.setheading(self.head.heading())
             draw_ht_turtle.forward(fraction_distance)
 
             # display fractional shift
@@ -88,18 +88,18 @@ class SnakeBody:
         later_segment = self.tail
         earlier_segment = later_segment.previous_segment
         while earlier_segment:
-            later_segment.set_heading(earlier_segment.get_heading())
+            later_segment.setheading(earlier_segment.heading())
             later_segment = earlier_segment
             earlier_segment = earlier_segment.previous_segment
 
     def _move_all_segments(self, grid_size):
         current_segment = self.head
         while current_segment:
-            current_segment.move(grid_size)
+            current_segment.forward(grid_size)
             current_segment = current_segment.next_segment
 
     def move(self, grid_size, screen):
-        self.head.set_heading(self.head_switch)
+        self.head.setheading(self.head_switch)
         self._draw_snake(screen)
         self._move_all_segments(grid_size)
         self._pass_directions()
