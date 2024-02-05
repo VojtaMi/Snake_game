bff1486ffee82e33821f4799db440f9ad7248e41
Vojtech Mikel <tami.vojkel@gmail.com>
2024-02-05 13:10:43 +0100

    fix bug with food generating on snakes body - coordinates values are rounded to compansate for float problems with precision

--- food.py
+++ food.py
@@ -7,7 +7,7 @@ def all_positions():
     possible_positions = []
     for xcor in range(-280, 280 + 1, 20):
         for ycor in range(-280, 280 + 1, 20):
-            possible_positions.append((xcor, ycor))
+            possible_positions.append((round(xcor), round(ycor)))
     return set(possible_positions)
 
 
@@ -21,6 +21,7 @@ class Food(Turtle):
         self.place(occupied_spots)
 
     def place(self, occupied_spots):
+        occupied_spots = {(round(x), round(y)) for x, y in occupied_spots}
         remaining_options = self.pos_options - set(occupied_spots)
         coordinates = random.choice(tuple(remaining_options))
         self.goto(coordinates)
