bd913302007d4533573faad30030a6ae1ec0ed79
Vojtech Mikel <tami.vojkel@gmail.com>
2024-02-20 12:40:48 +0100

    documentation

--- game.py
+++ game.py
@@ -6,12 +6,19 @@ from turtle import Turtle
 import time
 import leaderboard
 
-
 GRID_SIZE = 20
 
 
 class Game:
+    """
+    Game logic controller.
+    """
     def __init__(self, screen, input_handler):
+        """
+
+        :param screen: turtle screen where game is displayed
+        :param input_handler:  key listener from utils.py
+        """
         self.screen = screen
         self.snake = SnakeBody()
         self.food = Food(occupied_spots=self.snake.segments_positions())
@@ -22,18 +29,32 @@ class Game:
         game_loop = True
 
         while game_loop:
+            # check if snake's head and food are at the same spot, balancing float mistake
             if utils.check_proximity(self.snake.head.pos(), self.food.pos()):
-                self.snake.pin_segment(GRID_SIZE)
-                self.food.place(occupied_spots=self.snake.segments_positions())
-                self.score.increase()
-                self.score.display()
 
+                # when the food is eaten, the snake grows at its tail
+                self.snake.pin_segment(GRID_SIZE)
+
+                # try to place food at free spot, if no space is available, game ends
+                if self.food.place(occupied_spots=self.snake.segments_positions()):
+                    self.score.increase()
+                    self.score.display()
+                else:
+                    game_loop = False
+
+            # continue with the snake movement
             self.snake.move(GRID_SIZE, self.screen)
+
             if self.snake.collision():
                 game_loop = False
+
+            # checks for the flag from input_handler and sets the direction of snake's head
             self._move_on_key()
 
     def _move_on_key(self):
+        """
+        Checks for the flag from input_handler and sets the direction of snake's head
+        """
         input_key = self.input_handler.get_key_pressed()
         if input_key == "d":
             self.snake.turn(Direction.RIGHT)
@@ -44,37 +65,49 @@ class Game:
         if input_key == "s":
             self.snake.turn(Direction.DOWN)
 
-
     def end_game(self):
-        time.sleep(0.5)
+        """
+        display ending animation, check for high score
+        """
+
+        short_sleep = 0.3
+        medium_sleep = 0.5
+        long_sleep = 1.0
+
+        time.sleep(medium_sleep)
 
         # remove all elements from the screen
         self.snake.snake_animation.wipe_snake()
         self.food.hideturtle()
         self.screen.update()
 
-        time.sleep(0.3)
+        time.sleep(short_sleep)
 
+        # write "Game Over" on screen
         end_pen = GameOverWrite()
-        end_pen.write_game_over()
         self.screen.update()
 
-        time.sleep(1)
+        time.sleep(long_sleep)
 
+        # clear screen
         end_pen.clear()
         self.score.pen.clear()
 
+        # if the score is in top 5, add to the leaderboard
         leaderboard.add_if_high_score(self.score.score)
 
 
-
 class GameOverWrite(Turtle):
+    """
+    Displays "Game Over" on screen.
+    """
     def __init__(self):
         super().__init__()
         self.color("white")
         self.hideturtle()
+        self._write_game_over()
 
-    def write_game_over(self):
+    def _write_game_over(self):
         self.penup()
         self.goto(-160, -100)
         self.write("Game\nover", font=("Courier", 100, "normal"))
--- leaderboard.py
+++ leaderboard.py
@@ -6,6 +6,10 @@ TOP_PLAYERS = 5
 
 
 def _load_high_scores():
+    """
+    Saves score board from the file into a list
+    :return: List[Dict["player_name" = str,"score" = int]]
+    """
     high_scores_in_file = []
     if os.path.exists("high_scores.csv"):
         with open("high_scores.csv", "r") as file:
@@ -17,21 +21,51 @@ def _load_high_scores():
 
 
 def add_if_high_score(score):
+    """
+    Adds the score from the finished game to the leaderboard if it's high enough
+
+    Score is high enough if it's higher than the last score of the leaderboard,
+    if the leaderboard is not full, then it has to be at least higher than set minimal threshold (default 10)
+
+    :param score: final score at the end of a game session
+    """
+
+    # low scores are not saved,
+    min_score_threshold = 10
+    if score < min_score_threshold:
+        return
+
+    # read score table into a local list
     high_scores = _load_high_scores()
 
+    # the score is put into the leaderboard if the leaderboard is not yet full,
+    # or if it's higher than the last one in the leaderboard
     if len(high_scores) < TOP_PLAYERS or score > high_scores[-1]["score"]:
+        # prompt is displayed to ask player for the name
         player_name = turtle.textinput("Your score is in top 5", "Write your name to save your success")
+        # if no name is given, then default string "___" is subsidized
         if player_name is None or len(player_name) == 0:
             player_name = "___"
+        # long names are cut to fit on screen
         if len(player_name) > 10:
             player_name = player_name[:10]
         high_scores.append({"player_name": player_name, "score": int(score)})
+        # name and score are added to the leaderboard file
         _add_score_to_file(high_scores)
 
 
 def _add_score_to_file(high_scores):
+    """
+    Stores high scores in the correct order in the high_score file
+
+    Local list with high scores is sorted, so the newly added high score is at the right place
+    :param high_scores: unsorted list of leaderboard members
+
+    """
+    # sort the local list
     high_scores = _sort_and_cut(high_scores)
 
+    # rewrite the storing file
     with open("high_scores.csv", "w", newline='') as file:
         writer = csv.writer(file)
         for row in high_scores:
@@ -39,17 +73,27 @@ def _add_score_to_file(high_scores):
 
 
 def _sort_and_cut(high_scores):
+    """
+    Sorts local list of high scores, cuts it to the number of leaderboard members (default 5)
+    :param high_scores:
+    :return: standardized sorted leaderboard list
+    """
     high_scores = sorted(high_scores, key=lambda x: x['score'], reverse=True)
     resized = high_scores[:TOP_PLAYERS]
     return resized
 
 
 def score_lines():
+    """
+    Makes a formatted string from the leaderboard elements
+    :return: String
+    """
     high_scores = _load_high_scores()
     lines = ""
 
+    # each line of the leaderboard is added to the string, separated by "\n" element
     for i, record in enumerate(high_scores, start=1):
-        line = str(i) + "." + record['player_name'] + " " + str(record['score']) + "\n"
+        line = f"{i}.{record['player_name']} {record['score']}\n"
         lines += line
 
     return lines
