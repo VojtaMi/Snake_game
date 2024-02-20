32cc40b83caeab3004166bf95f4855e71cadacaa
Vojtech Mikel <tami.vojkel@gmail.com>
2024-02-20 12:46:13 +0100

    documentation

--- main.py
+++ main.py
@@ -9,18 +9,24 @@ SCREEN_HEIGHT = 660
 
 
 def setup_screen():
+    """
+     Set up the game screen.
+    :return: Screen: Initialized turtle screen.
+    """
     screen = Screen()
     screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
-    screen.bgcolor("#c19a6b")
     screen.title("Snake game")
     screen.tracer(0)
     return screen
 
+if __name__ == "__main__":
+    # set up the screen
+    screen = setup_screen()
+    # display background
+    background = Background()
 
-# Set up the screen
-screen = setup_screen()
-background = Background()
-main_menu = MainMenu(screen)
-main_menu.make_decision()
+    # start a menu decision loop
+    main_menu = MainMenu(screen)
+    main_menu.make_decision()
 
-turtle.mainloop()
+    screen.mainloop()
